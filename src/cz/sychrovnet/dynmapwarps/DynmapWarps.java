cd1c05baf63e2fa1db8b37fa8f61d78347edb0fb
stilett <stilett@atlas.cz>
2011-07-05 18:30:39 +0200

    Improved warp plugin detection and provider class loading.

--- src/cz/sychrovnet/dynmapwarps/DynmapWarps.java
+++ src/cz/sychrovnet/dynmapwarps/DynmapWarps.java
@@ -1,12 +1,17 @@
 package cz.sychrovnet.dynmapwarps;
 
 import java.io.File;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Logger;
 import cz.sychrovnet.dynmapwarps.providers.Provider;
 import cz.sychrovnet.dynmapwarps.providers.Warpz0rProvider;
 import org.bukkit.event.Event;
 import org.bukkit.event.server.PluginEnableEvent;
 import org.bukkit.event.server.ServerListener;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.util.config.Configuration;
 import org.dynmap.DynmapPlugin;
@@ -25,9 +30,10 @@ import java.util.List;
 public class DynmapWarps extends JavaPlugin {
     public static Logger log = Logger.getLogger("Minecraft");
     protected static final String LOG_PREFIX = "[DynmapWarps] ";
+    public Map<String, String> providerMap = new HashMap<String, String>();
 
     public DynmapPlugin dynmap;
-    public List<Provider> providers = new ArrayList<Provider>();
+    public Map<String, Provider> providers = new HashMap<String, Provider>();
     public Configuration configuration;
     public static File configurationFile;
 
@@ -48,11 +54,14 @@ public class DynmapWarps extends JavaPlugin {
             if (pluginName.equals("dynmap")) {
                 log.info(LOG_PREFIX + "dynmap plugin loaded");
                 addDynmapHandler();
-            } else if (pluginName.equals("Warpz0r")) {
-                log.info(LOG_PREFIX + "Warpz0r plugin loaded");
-                Provider warpz0r = new Warpz0rProvider(plugin);
-                providers.add(warpz0r);
-                warpz0r.onEnable();
+
+            } else {
+                for (Map.Entry<String, String> providerEntry : providerMap.entrySet()) {
+                    if (pluginName.equals(providerEntry.getKey())) {
+                        plugin.loadProvider(providerEntry.getKey(), providerEntry.getValue());
+                        break;
+                    }
+                }
             }
         }
     }
@@ -60,8 +69,8 @@ public class DynmapWarps extends JavaPlugin {
     public void onDisable() {
         removeDynmapHandler();
         dynmap = null;
-        for (Provider provider: providers) {
-            provider.onDisable();
+        for (Map.Entry<String, Provider> provider: providers.entrySet()) {
+            provider.getValue().onDisable();
         }
         providers.clear();
     }
@@ -93,6 +102,9 @@ public class DynmapWarps extends JavaPlugin {
         configurationFile = new File(this.getDataFolder(), "config.yml");
         configuration = new Configuration(configurationFile);
         loadConfig();
+        providerMap.clear();
+        providerMap.put("Warpz0r", "cz.sychrovnet.dynmapwarps.providers.Warpz0rProvider");
+        providers.clear();
 
         dynmap = (DynmapPlugin) getServer().getPluginManager().getPlugin("dynmap");
         if (dynmap == null) {
@@ -105,13 +117,22 @@ public class DynmapWarps extends JavaPlugin {
             addDynmapHandler();
         }
 
+        for (Map.Entry<String, String> providerEntry : providerMap.entrySet()) {
+            Plugin sourcePlugin = getServer().getPluginManager().getPlugin(providerEntry.getKey());
+
+            if (sourcePlugin != null && sourcePlugin.isEnabled()) {
+                this.loadProvider(providerEntry.getKey(), providerEntry.getValue());
+            }
+        }
+
         getServer().getPluginManager().registerEvent(Event.Type.PLUGIN_ENABLE, new Listener(this), Event.Priority.Low, this);
     }
 
 
     public List<Mark> getMarks() {
         List<Mark> marks = new ArrayList<Mark>();
-        for (Provider provider: providers) {
+        for (Map.Entry<String, Provider> providerEntry: providers.entrySet()) {
+            Provider provider = providerEntry.getValue();
             if (provider.isEnabled()) {
                 provider.setMarks(marks);
             }
@@ -125,9 +146,35 @@ public class DynmapWarps extends JavaPlugin {
         dynmap.getWebServer().handlers.put("/marks", new DynmapHandler(this, dynmap));
     }
 
+
     public void removeDynmapHandler() {
         if (dynmap.getWebServer() != null) {
             dynmap.getWebServer().handlers.remove("/marks");
+        }
+    }
+
+
+    public void loadProvider(String pluginName, String className) {
+        try {
+            Class[] parameterTemplate = new Class[1];
+            parameterTemplate[0] = DynmapWarps.class;
+
+            Class providerClass = Class.forName(className);
+            Constructor constructor = providerClass.getConstructor(parameterTemplate);
+
+            Object[] parameters = new Object[1];
+            parameters[0] = this;
+
+            Provider provider = (Provider) constructor.newInstance(parameters);
+            providers.put(pluginName, provider);
+            provider.onEnable();
+            log.info(LOG_PREFIX + pluginName + " plugin loaded");
+
+        } catch (ClassNotFoundException e) {
+            log.info(LOG_PREFIX + "Provider class " + className + " for " + pluginName + " not found");
+
+        } catch (Exception e) {
+            log.info(LOG_PREFIX + "Failed to load provider for " + pluginName);
         }
     }
 }
